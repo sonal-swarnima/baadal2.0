668b48e853e6cd447d80e5aee9dcdd990f9b11d8
Sonal Swarnima <sonals.visitor@cse.iitd.ernet.in>
2013-06-03 12:22:04 +0530

    Auth functionality committed

--- baadalinstallation/baadal/controllers/default.py
+++ baadalinstallation/baadal/controllers/default.py
@@ -1,3 +1,25 @@
-def index():
- return "Welcome to Baadal !!!"
+# -*- coding: utf-8 -*-
+###################################################################################
+# Added to enable code completion in IDE's.
+if 0:
+    from gluon import auth,T
+###################################################################################
 
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login 
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+def index():
+    return dict(message=T('Hello World'))
--- baadalinstallation/baadal/models/db.py
+++ baadalinstallation/baadal/models/db.py
@@ -1,46 +1,90 @@
 # -*- coding: utf-8 -*-
+###################################################################################
+# Added to enable code completion in IDE's.
+if 0:
+    from gluon import *  # @UnusedWildImport
+    from authuser import login_callback
+###################################################################################
 
-db = DAL('mysql://root:dbadmin@localhost/')
+import ConfigParser
+import os
 
-from gluon.tools import Auth
-auth = Auth(globals(),db)
+ctx_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..'))
 
-db.define_yable('user_vm_map',
-    Field('user_id', db.user),
-    Field('vm_id', db.vm_data))    
+config = ConfigParser.ConfigParser()
+config.read(os.path.join(ctx_path, 'static/config-db.cfg'));
+conn_str = config.get("MYSQL_CONF","mysql_conn")
+db = DAL(conn_str)
 
 db.define_table('constants',
-    Field('name','string',notnull=True),
-    Field('value1','string',notnull=True))
+    Field('name','string', notnull=True, unique=True),
+    Field('value','string',notnull=True))
 
 db.define_table('organisation',
-    Field('name','string',notnull=True),
-    Field('public_ip','string',length=15),
-    Field('details','string',))
+    Field('name','string', notnull=True),
+    Field('details','string'),
+    Field('public_ip','string',length=15), 
+    format='%(name)s (%(id)s)')
 
-db.define_table('role',
-    Field('role_id','id'),
-    Field('name','string',length=100,notnull=True),
-    Field('description','string',length=255),
-    primarykey=['role_id'])
+from gluon.tools import Auth
+auth = Auth(db)
 
-db.define_table('user',
-    Field('user_id','id'),
-    Field('first_name','string',length=128,notnull=True),
-    Field('last_name','string',length=128),
-    Field('email','string',length=128,notnull=True,requires=IS_EMAIL),
-    Field('username','string',length=30,notnull=True),
-    Field('password','string',length=512,notnull=True),
-    Field('organisation_id','integer',notnull=True),
-    Field('block_user','boolean',default=False,notnull=True),
-    primarykey=['user_id'])
+## configure custom auth tables
+auth.settings.table_user_name = 'user'
+auth.settings.table_group_name = 'user_group'
+auth.settings.table_membership_name = 'user_membership'
 
-db.define_table('user_role_map',
+## configure auth policy
+auth.settings.allow_basic_login = config.getboolean("AUTH_CONF","allow_basic_login")
+auth.settings.login_after_registration = config.getboolean("AUTH_CONF","login_after_registration")
+auth.settings.create_user_groups = config.get("AUTH_CONF","create_user_groups")
+auth.settings.actions_disabled=config.get("AUTH_CONF",config.get("AUTH_CONF","actions_disabled"))
+auth.settings.remember_me_form = config.getboolean("AUTH_CONF","remember_me_form")
+auth.settings.everybody_group_id= config.getint("AUTH_CONF","everybody_group_id")
+
+db.define_table(
+    auth.settings.table_user_name,
+    Field('first_name', length=128, default=''),
+    Field('last_name', length=128, default=''),
+    Field('email', length=128, default='', unique=True), # required
+    Field('username', length=128, default='', unique=True),
+    Field('password', 'password', length=512, readable=False, label='Password'), # required
+    Field('organisation_id', db.organisation, label='Organisation'),
+    Field('block_user','boolean',default=False, notnull=True, writable=False, readable=False),
+    Field('registration_key', length=512, writable=False, readable=False, default=''), # required
+    Field('reset_password_key', length=512, writable=False, readable=False, default=''), # required
+    Field('registration_id', length=512, writable=False, readable=False, default='')) # required
+
+custom_auth_table = db[auth.settings.table_user_name] # get the custom_auth_table
+custom_auth_table.first_name.requires =   IS_NOT_EMPTY(error_message=auth.messages.is_empty)
+custom_auth_table.last_name.requires =   IS_NOT_EMPTY(error_message=auth.messages.is_empty)
+custom_auth_table.email.requires = [
+  IS_EMAIL(error_message=auth.messages.invalid_email),
+  IS_NOT_IN_DB(db, custom_auth_table.email)]
+
+auth.settings.table_user = custom_auth_table # tell auth to use custom_auth_table
+
+auth.settings.table_group = db.define_table(
+    auth.settings.table_group_name,
+    Field('role','string',length=100,notnull=True, unique=True),
+    Field('description', length=255, default=''))
+
+auth.settings.table_membership = db.define_table(
+    auth.settings.table_membership_name,
     Field('user_id', db.user),
-    Field('role_id', db.role))
+    Field('group_id', db.user_group))
+
+###############################################################################
+auth.define_tables(username=True)
+###############################################################################
+if config.get("AUTH_CONF","auth_type") == 'ldap':
+    from gluon.contrib.login_methods.pam_auth import pam_auth
+    auth.settings.login_methods=[pam_auth()]
+    auth.settings.login_onaccept= [login_callback]  
+    
+###############################################################################
 
 db.define_table('host',
-    Field('host_id','id'),
     Field('host_ip','string',length=15,notnull=True),
     Field('mac_addr','string',length=100,notnull=True),
     Field('HDD','integer'),
@@ -48,11 +92,9 @@ db.define_table('host',
     Field('RAM','integer'),
     Field("category","string"),
     Field('status','integer'),
-    Field('vm_count','integer'),
-    primarykey=['host_id'])
+    Field('vm_count','integer'))
 
 db.define_table('datastore',
-    Field('datastore_id','id'),
     Field('ds_name','string'),
     Field('ds_ip','string',length=15),
     Field('path','string'),
@@ -61,19 +103,17 @@ db.define_table('datastore',
     Field('capacity','integer'))
 
 db.define_table('template',
-    Field('template_id','id'),
     Field('name','string',notnull=True),
     Field('os_type','string',length=30,notnull=True),
     Field('arch','string',notnull=True),
     Field('hdd','integer',notnull=True),
     Field('hdfile','string',notnull=True),
     Field('type','string',notnull=True),
-    Field('datastore_id',db.datastore,notnull=True),
-    primarykey=['template_id'])
+    Field('datastore_id',db.datastore))
 
 db.define_table('vm_data',
-    Field('vm_id','id'),
-    Field('vm_name','string',length=512,notnull=True),
+    Field('vm_name','string',length=512,notnull=True, unique=True),
+    Field('user_id',db.user),
     Field('host_id',db.host),
     Field('RAM','integer'),
     Field('HDD','integer'),
@@ -91,13 +131,17 @@ db.define_table('vm_data',
     Field('next_run_level','integer'),
     Field('start_time','time'),
     Field('end_time','time'),
-    Field('status','integer'),
-    primarykey=['vm_id'])
+    Field('status','integer'))
+
+db.define_table('user_vm_map',
+    Field('user_id', db.user),
+    Field('vm_id', db.vm_data),
+    primarykey=['user_id','vm_id'])
 
 db.define_table('vm_data_event',
-    Field('vm_event_id','id'),
     Field('vm_id',db.vm_data),
     Field('vm_name','string',length=512,notnull=True),
+    Field('user_id',db.user),
     Field('host_id',db.host),
     Field('RAM','integer'),
     Field('HDD','integer'),
@@ -115,33 +159,25 @@ db.define_table('vm_data_event',
     Field('next_run_level','integer'),
     Field('start_time','time'),
     Field('end_time','time'),
-    Field('status','integer'),
-    primarykey=['vm_event_id'])
+    Field('status','integer'))
 
 db.define_table('attached_disks',
-    Field('disk_id','id'),
     Field('vm_id',db.vm_data,notnull=True),
     Field('datastore_id',db.datastore,notnull=True),
-    Field('capacity','string',length=45),
-    primarykey=['disk_id'])
+    Field('capacity','string',length=45))
 
 db.define_table('snapshot',
-    Field('snapshot_id','id'),
     Field('vm_id',db.vm_data,notnull=True),
     Field('datastore_id',db.datastore,notnull=True),
-    Field('path','string',notnull=True),
-    primarykey=['snapshot_id'])
+    Field('path','string',notnull=True))
 
 db.define_table('task_queue',
-    Field('task_id','id'),
     Field('task_type','string',length=30,notnull=True),
     Field('vm_id',db.vm_data),
     Field('priority','integer',default=1, notnull=True),
-    Field('status','integer',notnull=True),
-    primarykey=['task_id'])
+    Field('status','integer',notnull=True))
 
 db.define_table('task_queue_event',
-    Field('task_event_id','id'),
     Field('task_id',db.task_queue,notnull=True),
     Field('task_type','string',length=30,notnull=True),
     Field('vm_id',db.vm_data,notnull=True),
@@ -149,17 +185,13 @@ db.define_table('task_queue_event',
     Field('error','string', length=512),
     Field('start_time','time',notnull=True),
     Field('end_time','time',notnull=True),
-    Field('attention_time','time',notnull=True),
-    primarykey=['task_event_id'])
+    Field('attention_time','time',notnull=True))
 
 db.define_table('vlan_map',
-    Field('vlan_id','id'),
     Field('vm_id',db.vm_data))
 
 db.define_table('vnc_server',
-    Field('vnc_server_id','id'),
-    Field('ip_addr','string',length=15,notnull=True),
-    primarykey=['vnc_server_id'])
+    Field('ip_addr','string',length=15,notnull=True))
 
 db.define_table('vnc_access',
     Field('vm_id',db.vm_data),
@@ -168,5 +200,15 @@ db.define_table('vnc_access',
     Field('duration','integer'),
     Field('time_requested','integer'))
 
+import json
+if not db(db.user_group).count():
+    items = json.loads(config.get('AUTH_CONF','group_startup_data'))
+    for val in items:
+        db.user_group.insert(role=val,description=items[val])
 
+if not db(db.organisation).count():
+    items = json.loads(config.get('AUTH_CONF','org_startup_data'))
+    for val in items:
+        db.organisation.insert(name=val,details=items[val])
 
+        
