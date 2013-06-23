d6b82b7dee6e23df8b6e4d2580aef89667edf63e
Apoorve Mohan <apoorvemohan@gmail.com>
2013-06-23 14:37:00 +0530

    remove __inti__.py from models

--- baadalinstallation/baadal/models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__all__ =['logger','get_add_template_form','get_add_host_form','get_add_datastore_form','get_configuration_elem','set_configuration_elem','get_create_vm_form','add_vm_request_to_queue','add_user_to_vm','add_to_cost','get_vm_list','get_all_vm_list','get_my_vm_list','approve_vm_request','get_vm_list','get_my_vm_list']
-
-from admin_model import *
-from common_model import *
-from faculty_model import *
-from user_model import *
