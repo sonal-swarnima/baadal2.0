b907ce77e062bb30a5f19a00016af9f90da72287
Sonal Swarnima <sonals.visitor@cse.iitd.ernet.in>
2013-07-23 18:23:01 +0530

    Menu visibility handling

--- baadalinstallation/baadal/models/menu.py
+++ baadalinstallation/baadal/models/menu.py
@@ -21,7 +21,7 @@ response.top_menu = [
     ]
 if auth.is_logged_in():
     response.user_menu = [
-        (H2('USER MENU'),False,None),
+        (H2('USER MENU'),False, dict(_href='#', _id='menu_user')),
         (T('Home'), False, URL('default','index')),
         (T('Request VM'), False, URL('user','request_vm')),
         (T('My VMs'), False, URL('user','list_my_vm')),
@@ -32,20 +32,20 @@ if auth.is_logged_in():
     
     if (is_moderator() | is_faculty()):
         response.faculty_menu = [
-            (H2('FACULTY MENU'),False,None),
+            (H2('FACULTY MENU'),False, dict(_href='#', _id='menu_faculty')),
             (T('Pending Requests {'+str(len(get_pending_requests()))+'} '), False, URL('faculty','pending_requests'))
             ]
             
         if (is_moderator() | is_orgadmin()):
             response.orgadmin_menu = [
-                (H2('ORG-ADMIN MENU'),False,None),
+                (H2('ORG-ADMIN MENU'),False, dict(_href='#', _id='menu_orgadmin')),
                 (T('List All Org-Level VMs'), False, URL('orgadmin','list_all_orglevel_vm')),
                 (T('Pending Org-Level VM Approvals {'+str(get_pending_approval_count())+'}'), False, URL('orgadmin','pending_approvals'))
                 ]
         
             if is_moderator():
                 response.admin_menu = [
-                    (H2('ADMIN MENU'),False,None),
+                    (H2('ADMIN MENU'),False, dict(_href='#', _id='menu_admin')),
                     (T('All VM''s'), False, URL('admin','list_all_vm')),
                     (T('Host and VMs'), False, URL('admin','hosts_vms')),
                     (T('Tasks'), False, URL('admin','task_list')),
@@ -53,7 +53,6 @@ if auth.is_logged_in():
                     (T('Emergency'), False, URL('default','page_under_construction')),
                     (T('Configure System'), False,dict(_href='#', _id='configure'),[
                         (T('Add Host'), False, URL('admin','host_details')),
-                        (T('Add Disk'), False, URL('default','page_under_construction')),
                         (T('Add Template'), False, URL('admin','add_template')),
                         (T('Add Datastore'), False, URL('admin','add_datastore'))
                         ])
