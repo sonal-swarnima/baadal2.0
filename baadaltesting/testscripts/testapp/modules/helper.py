cb7aa5bd8341bbb0fdd43a39c4e19fc17e801a9a
root <root@dell-OptiPlex-7010.(none)>
2013-12-25 10:09:50 +0530

    testing framework

--- baadaltesting/testscripts/testapp/modules/helper.py
+++ baadaltesting/testscripts/testapp/modules/helper.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python
 # coding: utf8
 import os
+import ConfigParser
+import MySQLdb as mdb
+import xml.etree.ElementTree as ET
 
-def xml_connect():
-	import xml.etree.ElementTree as ET
+
+def xml_connect():	
 	tree = ET.parse(os.path.join(get_context_path(),'static/testdata.xml'))# parsing the xml file
 	root = tree.getroot()
 	return root
 
-def get_config_file():
-    import ConfigParser
-    config = ConfigParser.ConfigParser()
-    config.read(os.path.join(get_context_path(),'static/db.conf'));
-    return config
 def get_context_path():
     ctx_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..'))
     return ctx_path 
     
 def db_connection():
-    import ConfigParser
-    import MySQLdb as mdb
+    
     Config = ConfigParser.ConfigParser()
     Config.read(os.path.join(get_context_path(),'static/db.conf'))
     host_ip=Config.get("db_info","host_ip")
