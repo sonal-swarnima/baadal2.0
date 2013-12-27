e6f507021c268c8523596448d5e187b1cd6ad379
Utkarsh <utkarshsins@gmail.com>
2013-12-27 08:45:33 +0530

    libvirt install

--- baadaltesting/sandbox/src/switch/install.sh
+++ baadaltesting/sandbox/src/switch/install.sh
@@ -5,7 +5,7 @@ function run
   package_install qemu-kvm
   package_install virtinst
 
-  #libvirt_install
+  libvirt_install
 
   virsh_force "net-destroy default"
   virsh_force "net-autostart --disable default"
@@ -44,67 +44,70 @@ function run
 
 function libvirt_install
 {
-  package_install libxml2-dev
-  package_install libgnutls-dev
-  package_install libyajl-dev
-  package_install libdevmapper-dev
-  package_install libcurl4-gnutls-dev
-  package_install python-dev
-  package_install libnl-dev
+  VERSION=$(virsh --version 2>/dev/null)
+  if [[ $VERSION != "1.0.0" ]]; then
+    package_install libxml2-dev
+    package_install libgnutls-dev
+    package_install libyajl-dev
+    package_install libdevmapper-dev
+    package_install libcurl4-gnutls-dev
+    package_install python-dev
+    package_install libnl-dev
 
-  dir=$pwd
+    dir=$pwd
 
-  mkdir -p $TEMP
-  cd $TEMP
+    mkdir -p $TEMP
+    cd $TEMP
 
-  $ECHO_PROGRESS "libvirt - extract"
-  tar -xvzf $LIBVIRT 1>>$LOGS/log.out 2>>$LOGS/log.err
-  status=$?
+    $ECHO_PROGRESS "libvirt - extract"
+    tar -xvzf $LIBVIRT 1>>$LOGS/log.out 2>>$LOGS/log.err
+    status=$?
 
-  if [[ $status -ne 0 ]]; then
-    $ECHO_ER libvirt - extract \(check logs\)
-    exit 1
-  else
-    $ECHO_OK libvirt - extract
-  fi
+    if [[ $status -ne 0 ]]; then
+      $ECHO_ER libvirt - extract \(check logs\)
+      exit 1
+    else
+      $ECHO_OK libvirt - extract
+    fi
 
-  cd libvirt-1.0.0
-  
-  $ECHO_PROGRESS "libvirt - configure"
-  ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc --with-esx=yes 1>>$LOGS/log.out 2>>$LOGS/log.err
-  status=$?
+    cd libvirt-1.0.0
+    
+    $ECHO_PROGRESS "libvirt - configure"
+    ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc --with-esx=yes 1>>$LOGS/log.out 2>>$LOGS/log.err
+    status=$?
 
-  if [[ $status -ne 0 ]]; then
-    $ECHO_ER libvirt - configure \(check logs\)
-    exit 1
-  else
-    $ECHO_OK libvirt - configure
-  fi
+    if [[ $status -ne 0 ]]; then
+      $ECHO_ER libvirt - configure \(check logs\)
+      exit 1
+    else
+      $ECHO_OK libvirt - configure
+    fi
 
-  $ECHO_PROGRESS "libvirt - make"
-  make 1>>$LOGS/log.out 2>>$LOGS/log.err
-  status=$?
+    $ECHO_PROGRESS "libvirt - make"
+    make 1>>$LOGS/log.out 2>>$LOGS/log.err
+    status=$?
 
-  if [[ $status -ne 0 ]]; then
-    $ECHO_ER libvirt - make \(check logs\)
-    exit 1
-  else
-    $ECHO_OK libvirt - make
-  fi
+    if [[ $status -ne 0 ]]; then
+      $ECHO_ER libvirt - make \(check logs\)
+      exit 1
+    else
+      $ECHO_OK libvirt - make
+    fi
 
-  $ECHO_PROGRESS "libvirt - make install"
-  make install 1>>$LOGS/log.out 2>>$LOGS/log.err
-  status=$?
+    $ECHO_PROGRESS "libvirt - make install"
+    make install 1>>$LOGS/log.out 2>>$LOGS/log.err
+    status=$?
 
-  if [[ $status -ne 0 ]]; then
-    $ECHO_ER libvirt - make install \(check logs\)
-    exit 1
-  else
-    $ECHO_OK libvirt - make install
-  fi
+    if [[ $status -ne 0 ]]; then
+      $ECHO_ER libvirt - make install \(check logs\)
+      exit 1
+    else
+      $ECHO_OK libvirt - make install
+    fi
 
-  service_restart libvirt-bin
+    service_restart libvirt-bin
 
-  cd $pwd
-  rm -rf $TEMP
+    cd $pwd
+    rm -rf $TEMP
+   fi
 }
