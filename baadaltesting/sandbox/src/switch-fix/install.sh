f691a8af768561991373ee75baf8900d250f1968
Utkarsh <utkarshsins@gmail.com>
2013-12-30 18:29:39 +0530

    Copy Baadal Installation Scripts to Controller

--- baadaltesting/sandbox/src/switch-fix/install.sh
+++
@@ -1,8 +0,0 @@
-function run
-{
-  route_del_net $ROUTE_GW 0.0.0.0 $OVS_BRIDGE 
-  ovsvsctl_del_port $OVS_ETHERNET
-  ovsvsctl_del_br $OVS_BRIDGE_EXTERNAL
-  ifconfig_noip $OVS_ETHERNET
-  dhclient $OVS_ETHERNET
-}
