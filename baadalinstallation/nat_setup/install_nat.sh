0c2f237a7bcbcb856517e235ba614460c417e558
Utkarsh <utkarshsins@gmail.com>
2014-01-22 19:19:07 +0530

    nat setup edited

--- baadalinstallation/nat_setup/install_nat.sh
+++ baadalinstallation/nat_setup/install_nat.sh
@@ -7,8 +7,8 @@ echo "BRCOMPAT=yes" >> /etc/default/openvswitch-switch
 service openvswitch-switch start
 module-assistant --non-inter --quiet auto-install openvswitch-datapath
 
-ovs-vsctl add-br br0
-ovs-vsctl add-port br0 $INTERNAL_INTERFACE
+ovs-vsctl add-br br1
+ovs-vsctl add-port br1 $INTERNAL_INTERFACE
 
 touch $BASE_PATH/ovs_postup.sh
 
@@ -24,7 +24,7 @@ if test $VLANS_IN_RANGE -eq 1; then
 	for ((i=$VLAN_START_RANGE;i<=$VLAN_END_RANGE;i++))
 	do
 		if test $i -ne $NAT_VLAN; then
-			ovs_str+="ovs-vsctl add-br vlan$i br0 $i\n"
+			ovs_str+="ovs-vsctl add-br vlan$i br1 $i\n"
 			iptables --table nat --append POSTROUTING --out-interface vlan$i -j MASQUERADE
 		fi
 		trunk_str+="$i,"	
@@ -34,18 +34,18 @@ else
 	for i in "${vlans[@]}"
 	do
 		if test $i -ne $NAT_VLAN; then
-                        ovs_str+="ovs-vsctl add-br vlan$i br0 $i\n"
+                        ovs_str+="ovs-vsctl add-br vlan$i br1 $i\n"
 			iptables --table nat --append POSTROUTING --out-interface vlan$i -j MASQUERADE
                 fi
 		trunk_str+="$i,"
 	done
 fi
-ovs_str+="ovs-vsctl set port br0 tag=$NAT_VLAN\n"
+ovs_str+="ovs-vsctl set port br1 tag=$NAT_VLAN\n"
 trunk_str=$(echo ${trunk_str:0:-1})
 
 echo -e "$ovs_str\novs-vsctl set port $INTERNAL_INTERFACE trunk=$trunk_str" > $BASE_PATH/ovs_postup.sh
 sed -i -e "s/iface\ lo\ inet\ loopback/iface\ lo\ inet\ loopback\nup\ service\ openvswitch-switch\ restart/" /etc/network/interfaces
-echo -e "\nauto $INTERNAL_INTERFACE\niface $INTERNAL_INTERFACE inet static\n\taddress 0.0.0.0\n\nauto br0\niface br0 inet dhcp" >> /etc/network/interfaces
+echo -e "\nauto $INTERNAL_INTERFACE\niface $INTERNAL_INTERFACE inet static\n\taddress 0.0.0.0\n\nauto br1\niface br1 inet static \n address $INTERNAL_IP \n netmask $NETMASK" >> /etc/network/interfaces
 
 while read line
 do
@@ -64,7 +64,7 @@ echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 DEBIAN_FRONTEND=noninteractive  apt-get install -y iptables-persistent
 
-iptables --table nat --append POSTROUTING --out-interface br0 -j MASQUERADE
+iptables --table nat --append POSTROUTING --out-interface br1 -j MASQUERADE
 echo 1 > /proc/sys/net/ipv4/ip_forward 
 /etc/init.d/iptables-persistent save
 /etc/init.d/iptables-persistent reload
