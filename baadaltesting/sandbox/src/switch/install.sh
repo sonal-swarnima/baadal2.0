373a0f84c9a11469f1a7856916019f90b58ca354
Utkarsh <utkarshsins@gmail.com>
2014-02-10 13:18:06 +0530

    Modularize

--- baadaltesting/sandbox/src/switch/install.sh
+++ baadaltesting/sandbox/src/switch/install.sh
@@ -9,17 +9,7 @@ function run
     
   libvirtd -d 1>>$LOGS/log.out 2>>$LOGS/log.err
 
-  dns=$(cat /var/run/dnsmasq/resolv.conf | sed "s:nameserver ::g")
-  ipcalc -b $dns | tee -a $LOGS/log.err | grep INVALID\ ADDRESS 1>>$LOGS/log.out
-  status=$?
-
-  if [[ $status -eq 0 ]]; then
-    $ECHO_ER Failed to retrieve DNS info from sandbox system \(check logs\)
-    tail -$LOG_SIZE $LOGS/log.err
-    exit 1
-  else
-    $ECHO_OK DNS = $dns
-  fi
+  dns_get
 
   ovsvsctl_del_br $OVS_BRIDGE_EXTERNAL
   ovsvsctl_del_br $OVS_BRIDGE_INTERNAL
@@ -105,166 +95,24 @@ function run
   $ECHO_OK Switch Installation Complete
 }
 
-function libvirt_install
+function dns_get
 {
-  VERSION=$(virsh --version 2>/dev/null)
-  if [[ $VERSION != "1.2.1" || ${LIBVIRT_INSTALL} == "yes" ]]; then
-    package_install libxml2-dev
-    package_install libgnutls-dev
-    package_install libyajl-dev
-    package_install libdevmapper-dev
-    package_install libcurl4-gnutls-dev
-    package_install python-dev
-    package_install libnl-dev
-    package_install libpciaccess-dev
-    package_install python
+  if [[ $DNS != '' ]]; then
+    dns=$DNS
+  else
+    dns=$(cat /var/run/dnsmasq/resolv.conf | sed "s:nameserver ::g")
+  fi
 
-    dir=$pwd
+  package_install ipcalc
+  ipcalc -b $dns | tee -a $LOGS/log.err | grep INVALID\ ADDRESS 1>>$LOGS/log.out
+  status=$?
 
-    mkdir -p $TEMP
-    cd $TEMP
-
-    $ECHO_PROGRESS "libvirt - extract"
-    tar -xvzf $LIBVIRT_TAR 1>>$LOGS/log.out 2>>$LOGS/log.err
-    status=$?
-
-    if [[ $status -ne 0 ]]; then
-      $ECHO_ER libvirt - extract \(check logs\)
-      tail -$LOG_SIZE $LOGS/log.err 
-      exit 1
-    else
-      $ECHO_OK libvirt - extract
-    fi
-
-    cd $LIBVIRT
-    
-    $ECHO_PROGRESS "libvirt - configure"
-    ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc 1>>$LOGS/log.out 2>>$LOGS/log.err
-    status=$?
-
-    if [[ $status -ne 0 ]]; then
-      $ECHO_ER libvirt - configure \(check logs\)
-      tail -$LOG_SIZE $LOGS/log.err 
-      exit 1
-    else
-      $ECHO_OK libvirt - configure
-    fi
-
-    file_backup $(which libvirtd)
-    kill_all libvirtd
-
-    $ECHO_PROGRESS "libvirt - make"
-    make 1>>$LOGS/log.out 2>>$LOGS/log.err
-    status=$?
-
-    if [[ $status -ne 0 ]]; then
-      $ECHO_ER libvirt - make \(check logs\)
-      tail -$LOG_SIZE $LOGS/log.err 
-      exit 1
-    else
-      $ECHO_OK libvirt - make
-    fi
-
-    $ECHO_PROGRESS "libvirt - make install"
-    make install 1>>$LOGS/log.out 2>>$LOGS/log.err
-    status=$?
-
-    if [[ $status -ne 0 ]]; then
-      $ECHO_ER libvirt - make install \(check logs\)
-      tail -$LOG_SIZE $LOGS/log.err 
-      exit 1
-    else
-      $ECHO_OK libvirt - make install
-    fi
-    
-    cd $pwd
-    rm -rf $TEMP
-
-
-    # Python-Bindings
-    dir=$pwd
-
-    cd $LIBVIRTPYTHON_DIR
-
-    $ECHO_PROGRESS "libvirt-python - build"
-    python setup.py build 1>>$LOGS/log.out 2>>$LOGS/log.err
-    status=$?
-
-    if [[ $status -ne 0 ]]; then
-      $ECHO_ER libvirt-python - build \(check logs\)
-      tail -$LOG_SIZE $LOGS/log.err 
-      exit 1
-    else
-      $ECHO_OK libvirt-python - build
-    fi
-
-    $ECHO_PROGRESS "libvirt-python - install"
-    python setup.py install 1>>$LOGS/log.out 2>>$LOGS/log.err
-    status=$?
-
-    if [[ $status -ne 0 ]]; then
-      $ECHO_ER libvirt-python - install \(check logs\)
-      tail -$LOG_SIZE $LOGS/log.err 
-      exit 1
-    else
-      $ECHO_OK libvirt-python - install
-    fi
-
-    cd $pwd
-
-   fi
-}
-
-function virtmanager_install
-{
-  VERSION=$(virt-install --version 2>&1)
-  if [[ $VERSION != "0.10.0" || ${VIRTMANAGER_INSTALL} == "yes" ]]; then
-    package_install gconf2
-    package_install librsvg2-common
-    package_install python
-    package_install python-appindicator
-    package_install python-dbus
-    package_install python-glade2
-    package_install python-gnome2
-    package_install python-gtk-vnc
-    package_install python-gtk2
-    package_install python-urlgrabber
-    package_install python-vte
-    package_install intltool
-    package_install libvirt-glib-1.0-dev
-
-    dir=$pwd
-
-    mkdir -p $TEMP
-    cd $TEMP
-
-    $ECHO_PROGRESS "virt-manager - extract"
-    tar -xvzf $VIRTMANAGER_TAR 1>>$LOGS/log.out 2>>$LOGS/log.err
-    status=$?
-
-    if [[ $status -ne 0 ]]; then
-      $ECHO_ER virt-manager - extract \(check logs\)
-      tail -$LOG_SIZE $LOGS/log.err 
-      exit 1
-    else
-      $ECHO_OK virt-manager - extract
-    fi
-
-    cd $VIRTMANAGER
-    
-    $ECHO_PROGRESS "virt-manager - install"
-    python setup.py install --prefix=/usr 1>>$LOGS/log.out 2>>$LOGS/log.err
-    status=$?
-
-    if [[ $status -ne 0 ]]; then
-      $ECHO_ER virt-manager - install \(check logs\)
-      tail -$LOG_SIZE $LOGS/log.err 
-      exit 1
-    else
-      $ECHO_OK virt-manager - install
-    fi
-
-    cd $pwd
-    rm -rf $TEMP
-   fi
+  if [[ $status -eq 0 ]]; then
+    $ECHO_ER Failed to retrieve DNS info from sandbox system \(check logs\) OR \
+      manually specify DNS as \'make switch DNS=a.b.c.d\'
+    tail -15 $LOGS/log.err
+    exit 1
+  else
+    $ECHO_OK DNS = $dns
+  fi
 }
