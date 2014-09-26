a1a808428e6743ef545890c24dfb67ecaa2a4e80
Sonal Swarnima <sonal.swarnima@yahoo.com>
2014-09-26 16:20:21 +0530

    Some comments added

--- baadalinstallation/baadal/models/trigger.py
+++ baadalinstallation/baadal/models/trigger.py
@@ -7,6 +7,16 @@ if 0:
     from task_scheduler import vm_scheduler
     from applications.baadal.models import *  # @UnusedWildImport
 ###################################################################################
+""" trigger.py:  This file has methods to handle db trigger requests.
+    Following handlers are defined:
+    Table              Trigger
+    vm_data            After Insert
+    vm_data            Before delete
+    task_queue         After Insert
+    task_queue         After Update
+    scheduler_task     After Update
+"""
+
 from helper import logger, get_datetime
 
 def schedule_task(fields, _id):
@@ -23,9 +33,17 @@ def schedule_task(fields, _id):
     if fields['task_type'] == TASK_TYPE_CLONE_VM:
         # In case of clone vm, Schedule as many task as the number of clones
         for clone_vm_id in fields['parameters']['clone_vm_id']:
-            vm_scheduler.queue_task('clone_task', pvars = dict(task_event_id = task_event_id, vm_id = clone_vm_id),start_time = request.now, timeout = 30 * MINUTES, group_name = 'vm_task')
+            vm_scheduler.queue_task('clone_task', 
+                                    pvars = dict(task_event_id = task_event_id, vm_id = clone_vm_id),
+                                    start_time = request.now, 
+                                    timeout = 30 * MINUTES, 
+                                    group_name = 'vm_task')
     else:
-        vm_scheduler.queue_task('vm_task', pvars = dict(task_event_id = task_event_id),start_time = request.now, timeout = 30 * MINUTES, group_name = 'vm_task')
+        vm_scheduler.queue_task('vm_task', 
+                                pvars = dict(task_event_id = task_event_id),
+                                start_time = request.now, 
+                                timeout = 5 * SECONDS, 
+                                group_name = 'vm_task')
 
 
 def vm_data_insert_callback(fields, _id):
@@ -51,14 +69,18 @@ db.task_queue._after_insert = [task_queue_insert_callback]
 
 
 def task_queue_update_callback(dbset, new_fields):
-#   If task queue status is set to retry, re-schedule the task
+
     if 'status' in new_fields:
         fields = dbset.select().first()
+        # If task queue status is set to retry, re-schedule the task
         if new_fields['status'] == TASK_QUEUE_STATUS_RETRY:
             schedule_task(fields,fields['id'])
+        # If task queue status is set to retry is set to failed, 
         elif new_fields['status'] == TASK_QUEUE_STATUS_FAILED:
+            # 1. update vm_data status to -1 if task is Create VM
             if fields['task_type'] == TASK_TYPE_CREATE_VM:
                 db.vm_data[fields['vm_id']] = dict(status = VM_STATUS_UNKNOWN)
+            # 2. update request_queue status to -1 if task has request_id
             if 'request_id' in fields['parameters']:
                 db.request_queue[fields['parameters']['request_id']] = dict(status = REQ_STATUS_FAILED)
 
@@ -83,38 +105,32 @@ def scheduler_task_update_callback(dbset, new_fields):
 
     if 'status' in new_fields and 'next_run_time' in new_fields:
         if new_fields['status']=='TIMEOUT':
-            logger.debug(new_fields['next_run_time'])
-            next_run_time = str(new_fields['next_run_time'])
-            next_run_time = next_run_time.split(".")
-            logger.debug(next_run_time[0])
-            rows =db((db.scheduler_task.status==new_fields['status']) & (db.scheduler_task.next_run_time==next_run_time[0])).select()
+            
+            rows = dbset.select()
             for row in rows:
-                logger.debug("status"+str(row.status))
-                logger.debug("next_run_time"+str(row.next_run_time))
-                logger.debug(row.vars)
                 if 'task_event_id' in row.vars:
-                    a_dict = ast.literal_eval(row.vars)
-                    logger.debug(a_dict)
-                    task_event_id1 = a_dict['task_event_id']
-                    logger.debug("Task TimedOut with task_event_id: "+ str(task_event_id1))
-                    task_timeout_cleanup(task_event_id1,row)
+                    param_dict = ast.literal_eval(row.vars)
+                    task_event_id = param_dict['task_event_id']
+                    logger.debug("Task TimedOut with task_event_id: "+ str(task_event_id))
+                    task_timeout_cleanup(task_event_id,row)
                 else:
                     logger.debug("Task TimedOut without cleanup")
-            
+          
 db.scheduler_task._after_update = [scheduler_task_update_callback]
 
 
 def task_timeout_cleanup(task_event_id, scheduler_row):
-    msg = ""
+    
     logger.debug("cleaning up for "+scheduler_row.status+" task: " + str(task_event_id))
     task_event_data = db.task_queue_event[task_event_id]
     task_queue_data = db.task_queue[task_event_data.task_id]
 
     if task_queue_data.status == TASK_QUEUE_STATUS_PENDING:
         logger.debug(task_queue_data.status)
-        #On return, update the status and end time in task event table^M
+        #On return, update the status and end time in task event table
+        msg = ""
         if scheduler_row.status == 'TIMEOUT':
-            msg = "Timeout"
+            msg = "Task Timeout"
         elif scheduler_row.status == 'FAILED':
             rows = db(db.scheduler_run.task_id==scheduler_row.id).select()
             rows.sort(lambda row: row.stop_time, reverse=True)
