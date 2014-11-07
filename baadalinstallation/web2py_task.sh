a5347b6370d133eae00f6b9079862e86a749a048
Sonal Swarnima <sonal.swarnima@yahoo.com>
2014-11-07 12:21:00 +0530

    scheduler command modified

--- baadalinstallation/web2py_task.sh
+++ baadalinstallation/web2py_task.sh
@@ -1,1 +1,3 @@
-python web2py.py -K baadal:vm_task,baadal:vm_sanity,baadal:host_task,baadal:vm_rrd,baadal:snapshot_task &
+#!/bin/bash
+
+sudo -u www-data python web2py.py -K baadal:vm_task,baadal:vm_sanity,baadal:host_task,baadal:vm_rrd,baadal:snapshot_task &
