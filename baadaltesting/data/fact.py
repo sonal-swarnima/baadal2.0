2f3a4b4b46e6c4dcf6d313ad03d28ba57486e18e
jyoti690 <jyoti690saini@gmail.com>
2015-03-29 17:16:01 +0530

    Update fact.py
    changed testing code

--- baadaltesting/data/fact.py
+++ baadaltesting/data/fact.py
@@ -1,12 +1,13 @@
+import os
 import datetime
 c_time=datetime.datetime.now()
-e_time=c_time + datetime.timedelta(seconds=60)
-print c_time
-print e_time
-fact=1
+e_time=c_time + datetime.timedelta(seconds=850)
 i=1
+fact=1
 print datetime.datetime.now()
 while(e_time>=datetime.datetime.now()):
-    fact=fact*i
-    i+=1
-print fact
+    #os.system("sysbench --test=mutex --memory-total-size=1G --memory-oper=read run ")
+   # os.system("sysbench --test=mutex --memory-total-size=1G --memory-oper=read cleanup ")
+   some_str = ' ' * 512000000
+
+
