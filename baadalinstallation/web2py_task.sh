909547b1ef7dafcb61e1bd4426fe692b7aaeb2f7
Sonal Swarnima <sonal.swarnima@yahoo.com>
2015-10-28 12:58:24 +0530

    Few issues fixed

--- baadalinstallation/web2py_task.sh
+++ baadalinstallation/web2py_task.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -u www-data python web2py.py -K baadal:vm_task,baadal:vm_sanity,baadal:host_task,baadal:vm_rrd,baadal:snapshot_task &
+su www-data -c "python web2py.py -K baadal:vm_task,baadal:vm_sanity,baadal:host_task,baadal:vm_rrd,baadal:snapshot_task &"
